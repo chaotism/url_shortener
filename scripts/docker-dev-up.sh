311f56bd1f8e067afd0f13feb203251f18c97d5b
Alexandr Bogut <chaotism@mail.ru>
2022-12-20 09:55:44 +0300

    refactor: feat-mvp cosmetic fixes

--- scripts/docker-dev-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-force_recreate=$1
-if [ ! "$force_recreate" ]
-then
-  docker-compose -f ../docker/docker-compose-dev.yml up -d --build
-else
-  docker-compose -f ../docker/docker-compose-dev.yml up -d --build --force-recreate
-fi
