cb78c0e6068043a082cdb5e5e8eb9f191e58b113
Alexandr Bogut <chaotism@mail.ru>
2023-04-25 00:09:50 +0300

    feat: update health check

--- application/server/routers/health/endpoints/app_info/handler.py
+++ application/server/routers/health/endpoints/app_info/handler.py
@@ -1,28 +1,31 @@
-from typing import Any
-
 from config import openapi_config
 from fastapi import APIRouter
 
+from common.utils import async_wrapper
+from clients import parser_client
+from dbs import mongo_adapter
 from .schemas import Msg
 
 router = APIRouter()
 
 
-@router.post(
-    '/system-status/', response_model=Msg, status_code=200
-)  # TODO: add checking chrome driver
-def check_app_status() -> Any:
+@router.post('/system-status/', response_model=Msg, status_code=200)
+async def check_app_status() -> Msg:
     """
     Check app status.
+    # Check Client
     # Check DB  # TODO: Not Implemented
     # Check application
     """
-    return {'msg': 'Ok'}
+
+    await async_wrapper(parser_client.get_page)('https://www.google.com/')
+    await mongo_adapter.get_db()
+    return Msg(**{'msg': 'Ok'})
 
 
 @router.post('/app-version/', response_model=Msg, status_code=200)
-def check_app_version() -> Any:
+async def check_app_version() -> Msg:
     """
     Check app version.
     """
-    return {'msg': openapi_config.version}
+    return Msg(**{'msg': openapi_config.version})
