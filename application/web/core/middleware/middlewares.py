deff81e88113d3b0a5cb3af3a5a4a6f85ec715e2
Alexandr Bogut <chaotism@mail.ru>
2023-11-13 18:27:08 +0300

    fix: change logs

--- application/web/core/middleware/middlewares.py
+++ application/web/core/middleware/middlewares.py
@@ -37,10 +37,10 @@ async def request_context_log_middleware(request: Request, call_next):
 
 
 async def request_logging_middleware(request: Request, call_next):
-    logger.info('request:  %s %s', request.method, request.url.path)
+    logger.info(f'request: {request.method} {request.url.path}')
     response = await call_next(request)
     logger.info(
-        'response %s %s %s', request.method, request.url.path, response.status_code
+        f'response {request.method} {request.url.path} {response.status_code}',
     )
     return response
 
@@ -58,8 +58,6 @@ async def logging_access_token(request: Request, call_next):
     receive = request.receive
     access_token = request.headers.get('Auth-Token', '')
     remote_ip = request.headers.get('X-Real-IP', '')
-    logger.info(
-        '%s to %s for IP %s with token %s', method, receive, remote_ip, access_token
-    )
+    logger.info(f'{method} to {receive} for IP {remote_ip} with token {access_token}')
     response = await call_next(request)
     return response
